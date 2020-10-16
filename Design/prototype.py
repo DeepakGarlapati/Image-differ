1c4fd809233c1f8f704310c9d8f8612ac60f3cac
Deepak Garlapati <venkatdeepak18@gmail.com>
2020-10-16 09:38:13 +0530

    Creating repo and adding source code

---
+++ Design/prototype.py
@@ -0,0 +1,6 @@
+## For all image in current working directory
+
+## Regex area name and timedate from the extracted text
+## Create a folder with the area name
+## Create a folder with time slots (6am - 12pm, 12pm - 6pm, 6pm - 12am, 12am - 6am)
+# If folder with area name found, move the image into the sub folder corresponding to time slot
